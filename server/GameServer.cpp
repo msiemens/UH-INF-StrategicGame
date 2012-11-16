25b4ab096bec06778db74581cb5222f40342368b
Armend <armend@meholli.com>
2012-11-16 11:53:22 +0100

    Created empty server class

---
+++ server/GameServer.cpp
@@ -0,0 +1,18 @@
+/*
+ * GameServer.cpp
+ *
+ *  Created on: 16.11.2012
+ *      Author: Armend
+ */
+
+#include "C:/GameDev/Projekte/UH-INF-StrategicGame/server/GameServer.h"
+
+GameServer::GameServer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GameServer::~GameServer() {
+	// TODO Auto-generated destructor stub
+}
+
