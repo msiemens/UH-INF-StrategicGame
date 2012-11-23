dac8cf9a8f343011a9ba4f244cd490b9b6054470
Henrik <henrikheymann@gmx.de>
2012-11-23 18:29:00 +0100

    GameEngine.h & GameEngine.cpp by Henrik

--- server/GameEngine.cpp
+++ server/GameEngine.cpp
@@ -5,14 +5,41 @@
  *      Author: Armend
  */
 
-#include "C:/GameDev/Projekte/UH-INF-StrategicGame/server/GameEngine.h"
+#include "server/GameEngine.h"
+#include "gamemodel/Player.h"
+#include "gamemodel/GameAction.h"
+
+using namespace std;
 
 GameEngine::GameEngine() {
 	// TODO Auto-generated constructor stub
-
+	isRunning=true;
+	playercount=0;
 }
 
 GameEngine::~GameEngine() {
 	// TODO Auto-generated destructor stub
+}
+
+bool GameEngine::onPlayerConnect(Player player){
+	playerlist.insert(playerlist.begin(),player);
+	playercount++;
+	return true;
+}
+
+void GameEngine::onPlayerDisconnect(Player player){
+	playerlist.remove(player);
+}
+
+void GameEngine::onPlayerAction(Player player,GameAction action){
+
+	if(logic.checkPlayerAction(player,action)==true){
+		createObject(player,action.what);
+	}
+
+}
+
+void GameEngine::createObject(Player player,GameEntity object){
+	player.addEntity(entity);
 }
 
--- server/GameEngine.h
+++ server/GameEngine.h
@@ -8,10 +8,34 @@
 #ifndef GAMEENGINE_H_
 #define GAMEENGINE_H_
 
+#include <list>
+#include "gamemodel/Player.h"
+#include "gamemodel/GameAction.h"
+#include "GameLogic.h"
+
+
+using namespace std;
+
 class GameEngine {
 public:
 	GameEngine();
 	virtual ~GameEngine();
+
+	bool gameIsRunning();
+
+
+	bool onPlayerConnect(Player player);
+	void onPlayerDisconnect(Player player);
+	void onPlayerAction(Player player,GameAction action);
+	void createObject(Player player,GameEntity action);
+
+	GameLogic logic;
+
+
+private:
+	bool isRunning;
+	list<Player> playerlist;
+	int playercount;
 };
 
 #endif /* GAMEENGINE_H_ */
