b7b5b20709dc8926f11dafeaa7256c52cf5d7307
Armend <armend@meholli.com>
2012-12-01 18:34:07 +0100

    Class CArea deleted GameClient.h,OnCleanup.cpp,OnEvent.cpp,OnInit.cpp,OnRender.cpp updated

--- client/OnRender.cpp
+++ client/OnRender.cpp
@@ -2,6 +2,6 @@
 
 using namespace std;
 void GameClient::OnRender() {
-    CArea::AreaControl.OnRender(Surf_Display, CCamera::CameraControl.GetX(), CCamera::CameraControl.GetY());
+    //CArea::AreaControl.OnRender(Surf_Display, CCamera::CameraControl.GetX(), CCamera::CameraControl.GetY());
     SDL_Flip(Surf_Display);
 }
