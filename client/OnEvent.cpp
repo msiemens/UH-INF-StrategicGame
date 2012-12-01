0491ac2626a8204208ed87475333a72d57643c16
Armend <armend@meholli.com+>
2012-12-01 18:57:14 +0100

    OnEvent.cpp updated

--- client/OnEvent.cpp
+++ client/OnEvent.cpp
@@ -9,22 +9,22 @@ void GameClient::OnExit() {
 	Running = false;
 }
 
-//void GameClient::OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) {
-//	switch (sym) {
-//	case SDLK_UP:
-//		CCamera::CameraControl.OnMove(0, 5);
-//		break;
-//	case SDLK_DOWN:
-//		CCamera::CameraControl.OnMove(0, -5);
-//		break;
-//	case SDLK_LEFT:
-//		CCamera::CameraControl.OnMove(5, 0);
-//		break;
-//	case SDLK_RIGHT:
-//		CCamera::CameraControl.OnMove(-5, 0);
-//		break;
-//
-//	default:
-//		break;
-//	}
-//}
+void GameClient::OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) {
+	switch (sym) {
+	case SDLK_UP:
+		// CCamera::CameraControl.OnMove(0, 5);
+		break;
+	case SDLK_DOWN:
+		// CCamera::CameraControl.OnMove(0, -5);
+		break;
+	case SDLK_LEFT:
+		// CCamera::CameraControl.OnMove(5, 0);
+		break;
+	case SDLK_RIGHT:
+		// CCamera::CameraControl.OnMove(-5, 0);
+		break;
+
+	default:
+		break;
+	}
+}
