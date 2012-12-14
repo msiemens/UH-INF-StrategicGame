4a7827ead952f61b097cf4edfeb6c5ea6f40815e
Armend <armend@meholli.com>
2012-12-14 12:21:10 +0100

    Edited: -CSurface.cpp -GameClient.h Deleted: -class CMap and CTile
    Function added:
    -Show Startscreen and Startmenu

--- client/CSurface.cpp
+++ client/CSurface.cpp
@@ -26,7 +26,7 @@ SDL_Surface* CSurface::OnLoad(char* File) {
 	SDL_Surface* Surf_Temp = NULL;
 	SDL_Surface* Surf_Return = NULL;
 
-	if ((Surf_Temp = IMG_Load(File)) == NULL) {
+	if ((Surf_Temp = SDL_LoadBMP(File)) == NULL) {
 		return NULL;
 	}
 
