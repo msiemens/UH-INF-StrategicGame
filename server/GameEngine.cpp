271113f2009fdb21391921a2181f7872173d7d7e
Armend <armend@meholli.com+>
2012-12-18 20:58:14 +0100

    added run() function.

--- server/GameEngine.cpp
+++ server/GameEngine.cpp
@@ -138,6 +138,7 @@ void GameEngine::doAction(PlayerPtr player, GameActionPtr action) {
 void GameEngine::run(){
 	 m_network.ConnectOnAction(boost::bind(&GameEngine::onPlayerAction, this, _1));
 	 m_network.thread()->detach();
+	 //test
 }
 
 void GameEngine::test() {
