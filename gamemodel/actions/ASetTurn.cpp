fe5f71cb185c850154abc6717a387c78fff9b05a
armend <armend@meholli.com>
2013-01-12 05:47:33 +0100

    add action setturn

---
+++ gamemodel/actions/ASetTurn.cpp
@@ -0,0 +1,13 @@
+/*
+ * ASetTurn.cpp
+ *
+ *  Created on: 12.01.2013
+ *      Author: Meholli
+ */
+
+#include "gamemodel/actions/ASetTurn.h"
+
+ASetTurn::~ASetTurn() {
+	// TODO Auto-generated destructor stub
+}
+
