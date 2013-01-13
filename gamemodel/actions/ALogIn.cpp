da7f1a05a422d62bbb9d4138c5ab6476bfa00765
armend <armend@meholli.com>
2013-01-14 00:16:31 +0100

    add action ALogIn

---
+++ gamemodel/actions/ALogIn.cpp
@@ -0,0 +1,13 @@
+/*
+ * ALogIn.cpp
+ *
+ *  Created on: 14.01.2013
+ *      Author: Meholli
+ */
+
+#include "gamemodel/actions/ALogIn.h"
+
+ALogIn::~ALogIn() {
+	// TODO Auto-generated destructor stub
+}
+
