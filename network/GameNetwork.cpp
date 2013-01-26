31f7996d891e465985fe72b14039282611ebfb27
Markus Siemens <siemens1993@gmail.com>
2013-01-26 14:48:31 +0100

    Removed unneccessary output and files

--- network/GameNetwork.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * GameNetwork.cpp
- *
- *  Created on: 08.11.2012
- *      Author: markus
- */
-
-#include "GameNetwork.h"
-
--- network/server/NetPlayer.cpp
+++ network/server/NetPlayer.cpp
@@ -26,17 +26,13 @@ tcp::socket& NetPlayer::socket() {
 
 // Start listening on our socket
 void NetPlayer::Start() {
-	cout << ":: Connection established" << endl;
 	m_game.Join(shared_from_this());
-	cout << "Player joined" << endl;
+	cout << "New Player joined" << endl;
 	_ReadHeader();
-	cout << "Waiting for header" << endl;
 }
 
 // Send a message to a player
 void NetPlayer::Send(NetworkMessagePtr msg) {
-	cout << "Delivering message..." << endl;
-
 	bool write_in_progress = !m_write_msgs.empty();
 	m_write_msgs.push_back(msg);
 	if (!write_in_progress) {
@@ -46,20 +42,16 @@ void NetPlayer::Send(NetworkMessagePtr msg) {
 
 // Handle an incoming header
 void NetPlayer::OnHeader(const boost::system::error_code& error) {
-	cout << "Recieving message..." << endl;
-
 	if (!error && m_read_msg->DecodeHeader()) {
 		_ReadBody();
 	} else {
-		cout << "Error: " << error.message() << endl;
+		cerr << "Error: " << error.message() << endl;
 		m_game.Leave(shared_from_this());
 	}
 }
 
 // Handle incoming body
 void NetPlayer::OnBody(const boost::system::error_code& error) {
-	cout << "Message recieved. Calling signal. " << endl;
-
 	if (!error) {
 		cout << "Message: "
 				<< string(m_read_msg->body(), m_read_msg->body_length()) << " ("
@@ -67,21 +59,20 @@ void NetPlayer::OnBody(const boost::system::error_code& error) {
 		m_signal_on_message(m_read_msg->body(), m_read_msg->body_length(), NetPlayerPtr(shared_from_this()));
 		_ReadHeader();
 	} else {
-		cout << "Error: " << error.message() << endl;
+		cerr << "Error: " << error.message() << endl;
 		m_game.Leave(shared_from_this());
 	}
 }
 
 // OnWriteHandler
 void NetPlayer::OnWrite(const boost::system::error_code& error) {
-	cout << "Message delivered" << endl;
 	if (!error) {
 		m_write_msgs.pop_front();
 		if (!m_write_msgs.empty()) {
 			_Write(m_write_msgs.front());
 		}
 	} else {
-		cout << "Error: " << error.message() << endl;
+		cerr << "Error: " << error.message() << endl;
 		m_game.Leave(shared_from_this());
 	}
 }
